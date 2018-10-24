5e8088ab1353817d08d8779399296d61e2f36b03
Wojciech Kocjan <wojciech@bitnami.com>
2018-10-24 14:53:37 +0200

    Update application to .NET Core 2.1

--- app/build-with-docker.sh
+++ app/build-with-docker.sh
@@ -7,6 +7,8 @@ if [ ! -d ./wwwroot ] ; then
   exit 2
 fi
 
+rm -f simple-asp-net-core-2-todo-app.zip
+
 docker run --rm -v "$(pwd):/src" -v /src/bin -v /src/obj microsoft/dotnet:2.1-sdk bash -c \
-  'apt-get update && apt-get -y install zip && cd /src && dotnet publish -c Release -r linux-x64 --self-contained=false && cd bin/Release/netcoreapp2.0/linux-x64/publish && zip -r -9 /src/simple-asp-net-core-2-todo-app.zip .'
+  'apt-get update && apt-get -y install zip && cd /src && dotnet publish -c Release -r linux-x64 --self-contained=false && cd bin/Release/netcoreapp2.1/linux-x64/publish && zip -r -9 /src/simple-asp-net-core-2-todo-app.zip .'
 
